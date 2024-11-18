ed34ed371859148fe6d68641680dd46ec091b623
Ebayart <elibayart05@gmail.com>
2024-11-18 15:27:01 +0100

    feat: add movement class to reduce Rover responsabilities

---
+++ src/Movement.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Movement {
+
+    public void forward(Direction direction, int x, int y) {
+
+    }
+
+    public void backward(Direction direction, int x, int y) {
+
+    }
+
+    public void left(Direction direction) {
+    }
+
+    public void right(Direction direction) {
+    }
+}
