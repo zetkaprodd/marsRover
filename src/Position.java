50dc73b112730b889ca94379f9b1a322ff654c21
Ebayart <elibayart05@gmail.com>
2024-11-27 11:54:37 +0100

    feat(position): use method to calculate replace whe rover is out of grid

--- src/Position.java
+++ src/Position.java
@@ -3,7 +3,8 @@ package src;
 public class Position {
 	int x;
 	int y;
-	
+	public static final int GRID_SIZE = 10;
+
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -21,8 +22,43 @@ public class Position {
 		this.y =y;
 	}
 
+	public void wrapAround(int gridSize) {
+		this.x = (this.x + GRID_SIZE) % GRID_SIZE;
+		this.y = (this.y + GRID_SIZE) % GRID_SIZE;
+	}
+
+
 	@Override
 	public String toString() {
 		return "Position [x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Position other = (Position) obj;
+		if (x != other.x) {
+			return false;
+		}
+		if (y != other.y) {
+			return false;
+		}
+		return true;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
 	}
 }
