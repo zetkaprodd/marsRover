408a212a7676e695ead543407c4068b6f9e2ea10
Ebayart <elibayart05@gmail.com>
2024-11-27 10:37:33 +0100

    feat(MissionController): add class MissionController

---
+++ src/MissionController.java
@@ -0,0 +1,13 @@
+package src;
+
+class MissionController {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void executeCommand() {
+        command.execute();
+    }
+}
