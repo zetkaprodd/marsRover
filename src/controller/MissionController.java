b5bfd28993d9306a1d462a55733a19c23becd3ee
Ebayart <elibayart05@gmail.com>
2024-11-28 08:33:29 +0100

    fix: resolve few sonarlint mistakes

--- src/controller/MissionController.java
+++ src/controller/MissionController.java
@@ -3,11 +3,6 @@ package src.controller;
 import src.command.Command;
 
 public class MissionController {
-    private Command command;
-
-    public void setCommand(Command command) {
-        this.command = command;
-    }
 
     public void executeCommand(Command command) {
         command.execute();
